6d67f0aab0856de53be20e863f0edcc276c7d713
Alex Cresswell <Alex.Cresswell@balfourbeatty.com>
2017-03-28 00:37:06 +0100

    Extra changes

--- TestDataGeneration/Tests.cs
+++ TestDataGeneration/Tests.cs
@@ -11,13 +11,29 @@ namespace TestDataGeneration
     {
         public static void Main()
         {
-            //Func<MyClass[], bool> rev = xs => xs.Reverse().Reverse().SequenceEqual(xs);
-            //Prop.ForAll(rev).VerboseCheck();
+            Func<int[], bool> rev = xs => xs.Reverse().Reverse().SequenceEqual(xs);
+            Prop.ForAll(Gen.ListOf(100, Gen.Choose(100, 200)).ToArbitrary(), x => x.Reverse().Reverse().SequenceEqual(x)).VerboseCheck();
 
-            Prop.ForAll<int>(a => new Func<bool>( () => 1/a == 1/a).When(a != 0) ).VerboseCheck();
+            Gen.ListOf(10, Gen.Choose(0, 1000));
 
-            var property = Prop.ForAll(MyClass.MyClasses(), mc => { return mc.Size <= 10; });
+            Gen.Choose(0, 100).Select(x => Gen.Choose(x, x + 100));
+
+            var l = Gen.Choose(0, 100).Sample(10, 1).ToList();
+            l.Sort();
+
+            foreach (var x in l)
+            {
+                Console.WriteLine(x);
+            }
+
+
+
+            Prop.ForAll<int>(a => new Func<bool>(() => 1 / a == 1 / a).When(a != 0)).VerboseCheck();
+
+            var property = Prop.ForAll(MyClass.MyClasses(), mc => { return mc != null; });
             property.VerboseCheck();
+
+            Prop.ForAll(MyClass.Choose(Enumerable.Range(0, 100).ToArray()).ToArbitrary(), x => x > 0).VerboseCheck();
 
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey(true);
